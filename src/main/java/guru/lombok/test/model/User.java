e6ff3a140d77a735cea2d5db0d659e82f089fd00
Pavlo Vozniuk <voznyuk.pavel.p@gmail.com>
2026-03-29 18:43:09 +0300

    lombok test

---
+++ src/main/java/guru/lombok/test/model/User.java
@@ -0,0 +1,16 @@
+package guru.lombok.test.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
