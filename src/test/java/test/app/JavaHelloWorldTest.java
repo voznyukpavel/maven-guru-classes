37157f3495e401370c676dead866aa26fe463988
Pavlo Vozniuk <voznyuk.pavel.p@gmail.com>
2026-04-25 14:55:31 +0300

    release plugin

---
+++ src/test/java/test/app/JavaHelloWorldTest.java
@@ -0,0 +1,8 @@
+package test.app;
+
+public class JavaHelloWorldTest {
+        @org.junit.jupiter.api.Test
+         void getHelloTest() {
+            System.out.println(new main.app.JavaHelloWorld().getHelloWorld());
+        }
+}
